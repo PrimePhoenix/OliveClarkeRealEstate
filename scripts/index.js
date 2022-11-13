1cd95201fdf82f691ad0b000dcfe10995c01cadd
PrimePhoenix <rohan.powell36@yahoo.com>
2022-11-13 16:07:52 -0500

    review page added Firebase databse added to store user comments broken links to review page were fixed index.js file created to store js code rather than crowd the target page with scripts

---
+++ scripts/index.js
@@ -0,0 +1,59 @@
+
+        const form=document.getElementById("reviewform");
+        var comments=[];
+
+              
+        function loadComments2(comm)
+        {
+            temp=document.getElementById("Review");
+            
+            
+        
+        if(document.getElementById("Nocomments").style.display=="none"&&comm==null)
+        {
+            document.getElementById("Nocomments").style.display="block";
+        }else{
+        document.getElementById("Nocomments").style.display="none";
+            
+       
+        newComment=document.createElement("li");
+        newComment.innerText=comm.Comment;
+        CommentData=document.createElement("div");
+        CommentData.innerText=comm.Name+" posted on "+comm.DatePosted;
+        newComment.appendChild(CommentData);
+        
+        temp.appendChild(newComment);
+            
+        
+        }
+        }
+        
+        function addReview(){
+        document.querySelector("#container").style.display="none";
+        document.querySelector("#newRating").style.display="block";
+        }
+        
+document.getElementById("cancelcomment").addEventListener('click',(e)=>{
+    
+    
+        e.preventDefault();
+        confirmReset=confirm("Are you sure you want to Cancel?");
+
+        
+        if(confirmReset==true)
+        {
+            form.reset();
+            document.querySelector("#container").style.display="block";
+        document.querySelector("#newRating").style.display="none";
+        }
+    })
+       
+
+           
+
+            
+           
+
+           
+
+
